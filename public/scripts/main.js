01c955286912bd45712b093ffcb1683773fab5c3
davidaxel121 <davidaxel121@gmail.com>
2021-03-01 08:38:52 -0600

    First commit with flex template and navbar working correctly

---
+++ public/scripts/main.js
@@ -0,0 +1,9 @@
+function toggleDarkMode( ) {
+	const element = document.getElementById("html-provider");
+  	element.classList.toggle("dark");
+}
+
+function openOrCloseNavbar() {
+	const element = document.getElementById("navbarElements");
+	element.classList.toggle("hidden");
+}
